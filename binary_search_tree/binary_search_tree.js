43f81692bcf24ac1d6b74300d22e52a6c4478008
stephenkaraga <stephenkaraga@gmail.com>
2015-05-25 19:40:18 -0400

    update tree

---
+++ binary_search_tree/binary_search_tree.js
@@ -0,0 +1,30 @@
+function BinarySearchTree(){
+	this._root = null;
+}
+
+BinarySearchTree.prototype = {
+	constructor: BinarySearchTree,
+
+	add: function(value){
+
+	},
+
+	contains: function(value){
+
+	}, 
+
+	remove: function(value){
+
+	},
+
+	size: function(value){
+
+	},
+
+	toArray: function(){
+	},
+
+	toString: function(){
+
+	}
+};
